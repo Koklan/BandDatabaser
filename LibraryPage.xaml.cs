c4da3160b97a1698e8c97d409e9990bac4e70077
Nikola Sarančuk <xsaran03@vutbr.cz>
2021-04-15 10:05:41 +0200

    Add project files.

---
+++ LibraryPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BandDatabaser
+{
+    /// <summary>
+    /// Interaction logic for LibraryPage.xaml
+    /// </summary>
+    public partial class LibraryPage : Page
+    {
+        public LibraryPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Thickness margin = SearchBtn.Margin;
+            margin.Left = 174 + BandText.ActualWidth;
+            SearchBtn.Margin = margin;
+        }
+    }
+}
